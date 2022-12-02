65af9309f6b86534dc144fe57cd2f8733a82e602
choeqq <k.chola@op.pl>
2022-12-02 19:07:34 +0100

    day-1 part two

---
+++ src/2022/1/2.ts
@@ -0,0 +1,22 @@
+import fs from 'fs';
+
+const input = fs.readFileSync('input.txt', 'utf8');
+const inputArray = input.split('\n');
+
+let currentCount = 0;
+const totals: number[] = [];
+
+inputArray.forEach((line) => {
+	if (line == '') {
+		totals.push(currentCount);
+		currentCount = 0;
+	} else {
+		currentCount += Number(line);
+	}
+});
+
+totals.sort((a, b) => b - a);
+
+const total = totals[0] + totals[1] + totals[2];
+
+console.log(total);
