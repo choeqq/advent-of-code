46c8d4aaaecbd05344dda18cf180d2dc0163eec4
choeqq <k.chola@op.pl>
2023-01-02 23:32:55 +0100

    day 14 part 1 fix

--- src/2022/14/1.ts
+++ src/2022/14/1.ts
@@ -1,92 +1,73 @@
-const fs = require('fs');
-const assert = require('assert');
+import * as fs from 'fs';
 
-function placeWalls(
-	data: string,
-	floor = false
-): [Map<string, '#' | 'o'>, number] {
-	let greatestY = -Infinity;
+type Position = [number, number];
 
-	const world = new Map<string, '#' | 'o'>();
-	for (const coordList of data.split('\n')) {
-		const coords = coordList
-			.split(' -> ')
-			.map((rawCoord) => rawCoord.split(',').map(Number));
-		let [x, y] = coords[0];
-		world.set(`${x},${y}`, '#');
-		for (const [x2, y2] of coords.slice(1)) {
-			if (y2 > greatestY) greatestY = y2;
-			while (x !== x2 || y !== y2) {
-				if (x !== x2) x += x < x2 ? 1 : -1;
-				else if (y !== y2) y += y < y2 ? 1 : -1;
-				world.set(`${x},${y}`, '#');
+const spawnPos = [500, 0] as Position;
+const maxDist = 1000;
+
+const nextPos = (curr: Position, occupied: Set<string>): Position | 'stop' => {
+	const down = [curr[0], curr[1] + 1] as Position;
+	const downLeft = [curr[0] - 1, curr[1] + 1] as Position;
+	const downRight = [curr[0] + 1, curr[1] + 1] as Position;
+	if (!occupied.has(down.toString())) return down;
+	if (!occupied.has(downLeft.toString())) return downLeft;
+	if (!occupied.has(downRight.toString())) return downRight;
+	return 'stop';
+};
+
+const input = fs
+	.readFileSync('inputs/day14.txt', 'utf8')
+	.split('\n')
+	.filter((l) => l.length > 0);
+
+const paths = input.flatMap((inp) => {
+	const lines = inp.split(' -> ');
+	const points: Position[] = lines.map(
+		(l) => l.split(',').map(Number) as Position
+	);
+
+	return points.reduce(
+		(tally: Position[], curr: Position, idx: number): Position[] => {
+			if (idx < 1) return tally;
+			const prev = points[idx - 1];
+			if (curr[0] === prev[0]) {
+				const howMany = Math.abs(curr[1] - prev[1]) + 1;
+				return [
+					...tally,
+					...(Array.from(
+						{ length: howMany },
+						(_, i) => i + Math.min(curr[1], prev[1])
+					).map((a) => [curr[0], a]) as Position[]),
+				];
+			} else {
+				const howMany = Math.abs(curr[0] - prev[0]) + 1;
+				return [
+					...tally,
+					...(Array.from(
+						{ length: howMany },
+						(_, i) => i + Math.min(curr[0], prev[0])
+					).map((a) => [a, curr[1]]) as Position[]),
+				];
 			}
-		}
-	}
-
-	if (floor) {
-		greatestY += 2;
-		for (let x = -1000; x < 1000; x++) world.set(`${x},${greatestY}`, '#');
-	}
-
-	return [world, greatestY];
-}
-
-function simulateSand(world: Map<string, '#' | 'o'>, greatestY: number) {
-	sandFalling: while (true) {
-		let [x, y] = [500, 0];
-		if (world.get(`${x},${y}`) === 'o') break;
-		while (true) {
-			if (y >= greatestY) {
-				world.delete(`${x},${y}`);
-				break sandFalling;
-			} else if (world.get(`${x},${y}`) === undefined) {
-				world.set(`${x},${y}`, 'o');
-			} else if (world.get(`${x},${y + 1}`) === undefined) {
-				world.delete(`${x},${y}`);
-				y++;
-				world.set(`${x},${y}`, 'o');
-			} else if (world.get(`${x - 1},${y + 1}`) === undefined) {
-				world.delete(`${x},${y}`);
-				x--;
-				y++;
-				world.set(`${x},${y}`, 'o');
-			} else if (world.get(`${x + 1},${y + 1}`) === undefined) {
-				world.delete(`${x},${y}`);
-				x++;
-				y++;
-				world.set(`${x},${y}`, 'o');
-			} else break;
-		}
-	}
-
-	return [...world.values()].filter((value: any) => value === 'o').length;
-}
-
-function solveOne(data: string): any {
-	return simulateSand(...placeWalls(data, false));
-}
-
-(() => {
-	const data = fs.readFileSync(__dirname + '/input.in').toString();
-	assert.deepStrictEqual(
-		solveOne(`498,4 -> 498,6 -> 496,6
-503,4 -> 502,4 -> 502,9 -> 494,9`),
-		24
+		},
+		[]
 	);
-	console.log(solveOne(data));
-})();
+});
 
-function solveTwo(data: string): any {
-	return simulateSand(...placeWalls(data, true));
-}
+const blocked = new Set(paths.map((p) => p.toString()));
+const sand = new Set();
 
-(() => {
-	const data = fs.readFileSync(__dirname + '/input.in').toString();
-	assert.deepStrictEqual(
-		solveTwo(`498,4 -> 498,6 -> 496,6
-503,4 -> 502,4 -> 502,9 -> 494,9`),
-		93
-	);
-	//console.log(solveTwo(data));
-})();
+let falling = true;
+do {
+	let sandPos = spawnPos;
+	let distanceFallen = 0;
+	while (nextPos(sandPos, blocked) !== 'stop' && distanceFallen < maxDist) {
+		sandPos = nextPos(sandPos, blocked) as Position;
+		distanceFallen++;
+	}
+	blocked.add(sandPos.toString());
+	sand.add(sandPos.toString());
+	falling = distanceFallen !== maxDist;
+} while (falling);
+
+console.log(sand.size - 1);
