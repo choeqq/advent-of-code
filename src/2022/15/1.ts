15c67f5194f63f861e6402cec8e8a760bc36b481
choeqq <k.chola@op.pl>
2023-01-02 23:33:38 +0100

    day 15 part 1 fix

--- src/2022/15/1.ts
+++ src/2022/15/1.ts
@@ -1,116 +1,71 @@
-const fs = require('fs');
-const assert = require('assert');
+import * as fs from 'fs';
 
-const parseSensorInfo = (data: string) => {
-	return data.split('\n').map((line) => {
-		const [sx, sy] = line
-			.match(/x=(-?\d+), y=(-?\d+)/)!
-			.slice(1)
-			.map(Number);
-		const [bx, by] = line
-			.split('beacon')[1]
-			.match(/x=(-?\d+), y=(-?\d+)/)!
-			.slice(1)
-			.map(Number);
-		return { sx, sy, bx, by, dist: Math.abs(sx - bx) + Math.abs(sy - by) };
-	});
+type Position = [number, number];
+
+type Sensor = {
+	pos: Position;
+	beacon: Position;
+	view: number;
 };
 
-function solveOne(data: string, y: number): any {
-	let count = 0;
+const targetY = 2000000;
 
-	const seenXs = new Set();
-	for (const { sx, sy, bx, by, dist } of parseSensorInfo(data)) {
-		if (sy === y) seenXs.add(sx);
-		if (by === y) seenXs.add(bx);
+const calculateDistance = (from: Position, to: Position): number => {
+	return Math.abs(from[0] - to[0]) + Math.abs(from[1] - to[1]);
+};
 
-		for (let x = sx - dist; x <= sx + dist; x++) {
-			if (!seenXs.has(x) && Math.abs(sx - x) + Math.abs(sy - y) <= dist) {
-				seenXs.add(x);
-				count++;
-			}
-		}
-	}
+const sum = (a: number, b: number): number => a + b;
 
-	return count;
-}
+const sensors: Sensor[] = fs
+	.readFileSync('inputs/day15.txt', 'utf8')
+	.split('\n')
+	.filter((line) => line.length > 0)
+	.map((line) => {
+		const regex = /(x=(-?\d+), y=(-?\d+))/g;
+		const match1 = regex.exec(line) || [];
+		const sensorPos = [
+			match1[2] ? Number(match1[2]) : 0,
+			match1[3] ? Number(match1[3]) : 0,
+		] as Position;
+		const match2 = regex.exec(line) || [];
+		const beaconPos = [
+			match2[2] ? Number(match2[2]) : 0,
+			match2[3] ? Number(match2[3]) : 0,
+		] as Position;
+		return {
+			pos: sensorPos,
+			beacon: beaconPos,
+			view:
+				Math.abs(sensorPos[0] - beaconPos[0]) +
+				Math.abs(sensorPos[1] - beaconPos[1]),
+		} as Sensor;
+	});
 
-(() => {
-	const data = fs.readFileSync(__dirname + '/input.in').toString();
-	assert.deepStrictEqual(
-		solveOne(
-			`Sensor at x=2, y=18: closest beacon is at x=-2, y=15
-Sensor at x=9, y=16: closest beacon is at x=10, y=16
-Sensor at x=13, y=2: closest beacon is at x=15, y=3
-Sensor at x=12, y=14: closest beacon is at x=10, y=16
-Sensor at x=10, y=20: closest beacon is at x=10, y=16
-Sensor at x=14, y=17: closest beacon is at x=10, y=16
-Sensor at x=8, y=7: closest beacon is at x=2, y=10
-Sensor at x=2, y=0: closest beacon is at x=2, y=10
-Sensor at x=0, y=11: closest beacon is at x=2, y=10
-Sensor at x=20, y=14: closest beacon is at x=25, y=17
-Sensor at x=17, y=20: closest beacon is at x=21, y=22
-Sensor at x=16, y=7: closest beacon is at x=15, y=3
-Sensor at x=14, y=3: closest beacon is at x=15, y=3
-Sensor at x=20, y=1: closest beacon is at x=15, y=3`,
-			10
-		),
-		26
-	);
-	console.log(solveOne(data, 2000000));
-})();
+const scanRange = sensors.reduce(
+	(currRange, currSensor): [number, number] => {
+		return [
+			Math.min(currRange[0], currSensor.pos[0] - currSensor.view),
+			Math.max(currRange[1], currSensor.pos[0] + currSensor.view),
+		];
+	},
+	[Number.MAX_VALUE, Number.MIN_VALUE]
+);
 
-function solveTwo(data: string, mxy: number): any {
-	const sensorData = parseSensorInfo(data);
+const xCoords = Array.from(
+	{ length: Math.abs(scanRange[1] - scanRange[0]) + 1 },
+	(_, i) => i + scanRange[0]
+);
 
-	const isOurOfAllSensorRanges = (x: number, y: number) =>
-		sensorData.every(
-			({ sx, sy, dist }) => Math.abs(sx - x) + Math.abs(sy - y) > dist
+const targetRow: number[] = xCoords.map((targetX) => {
+	const whichSensorSeesThis = sensors.findIndex((sensor) => {
+		return (
+			calculateDistance(sensor.pos, [targetX, targetY]) <= sensor.view &&
+			(sensor.beacon[0] !== targetX || sensor.beacon[1] !== targetY)
 		);
+	});
+	return whichSensorSeesThis === -1 ? 0 : 1;
+});
 
-	for (const { sx, sy, dist } of sensorData) {
-		for (const xo of [-1, 1]) {
-			for (const yo of [-1, 1]) {
-				for (let dx = 0; dx <= dist + 1; dx++) {
-					const dy = dist + 1 - dx;
-					const x = sx + dx * xo;
-					const y = sy + dy * yo;
-					if (
-						x >= 0 &&
-						y >= 0 &&
-						x <= mxy &&
-						y <= mxy &&
-						isOurOfAllSensorRanges(x, y)
-					) {
-						return x * 4000000 + y;
-					}
-				}
-			}
-		}
-	}
-}
+const answer = targetRow.reduce(sum);
 
-(() => {
-	const data = fs.readFileSync(__dirname + '/input.in').toString();
-	assert.deepStrictEqual(
-		solveTwo(
-			`Sensor at x=2, y=18: closest beacon is at x=-2, y=15
-	Sensor at x=9, y=16: closest beacon is at x=10, y=16
-	Sensor at x=13, y=2: closest beacon is at x=15, y=3
-	Sensor at x=12, y=14: closest beacon is at x=10, y=16
-	Sensor at x=10, y=20: closest beacon is at x=10, y=16
-	Sensor at x=14, y=17: closest beacon is at x=10, y=16
-	Sensor at x=8, y=7: closest beacon is at x=2, y=10
-	Sensor at x=2, y=0: closest beacon is at x=2, y=10
-	Sensor at x=0, y=11: closest beacon is at x=2, y=10
-	Sensor at x=20, y=14: closest beacon is at x=25, y=17
-	Sensor at x=17, y=20: closest beacon is at x=21, y=22
-	Sensor at x=16, y=7: closest beacon is at x=15, y=3
-	Sensor at x=14, y=3: closest beacon is at x=15, y=3
-	Sensor at x=20, y=1: closest beacon is at x=15, y=3`,
-			20
-		),
-		56000011
-	);
-	console.log(solveTwo(data, 4000000));
-})();
+console.log(answer);
