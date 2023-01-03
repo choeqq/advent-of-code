4fce3ca205999a70cb6f5688d78bcd5ddd2b3241
choeqq <k.chola@op.pl>
2023-01-03 23:19:51 +0100

    day 16 part 1 fix

--- src/2022/15/2.ts
+++ src/2022/15/2.ts
@@ -1,89 +1,177 @@
 import * as fs from 'fs';
 
-type Position = [number, number];
-
-type Sensor = {
-	pos: Position;
-	beacon: Position;
-	view: number;
+type Room = {
+	name: string;
+	flow: number;
+	adj: string[];
 };
 
-const maxCoord = 4000000;
-
-const calculateDistance = (from: Position, to: Position): number => {
-	return Math.abs(from[0] - to[0]) + Math.abs(from[1] - to[1]);
+type InputRoomMap = {
+	[name: string]: Room;
 };
 
-const getPerimetre = (from: Sensor): Position[] => {
-	const centre = from.pos;
-	const topRight = Array.from(
-		{ length: from.view + 2 },
-		(_, i) => [centre[0] + i, centre[1] - from.view + i - 1] as Position
-	);
-	const bottomRight = Array.from(
-		{ length: from.view + 2 },
-		(_, i) => [centre[0] + i, centre[1] + from.view - i + 1] as Position
-	);
-	const bottomLeft = Array.from(
-		{ length: from.view + 2 },
-		(_, i) => [centre[0] - i, centre[1] + from.view - i + 1] as Position
-	);
-	const topLeft = Array.from(
-		{ length: from.view + 2 },
-		(_, i) => [centre[0] - i, centre[1] - from.view + i - 1] as Position
-	);
-	return [...topRight, ...bottomRight, ...bottomLeft, ...topLeft];
+type CostMap = {
+	[name: string]: number;
 };
 
-const sensors: Sensor[] = fs
-	.readFileSync('inputs/day15.txt', 'utf8')
+type PricesRoomMap = {
+	[name: string]: CostMap;
+};
+
+type Path = {
+	curr: string;
+	toVisit: string[];
+	timeLeft: number;
+	finished: boolean;
+	steps: string[];
+	finalPressure: number;
+};
+
+const sortPathByPressure = (a: Path, b: Path) =>
+	b.finalPressure - a.finalPressure;
+
+const getMaxPressure = (
+	time: number,
+	destinations: Room[],
+	priceMap: PricesRoomMap,
+	roomsFromName: InputRoomMap
+) => {
+	const paths: Path[] = [
+		{
+			curr: 'AA',
+			toVisit: destinations.map((r) => r.name),
+			timeLeft: time,
+			finished: false,
+			steps: [],
+			finalPressure: 0,
+		},
+	];
+
+	for (let n = 0; n < paths.length; n++) {
+		const path = paths[n];
+		if (path.timeLeft <= 0 || path.finished) {
+			path.finished = true;
+			continue;
+		}
+
+		const currPrices = priceMap[path.curr];
+		let madeNewPath = false;
+		path.toVisit.forEach((room) => {
+			if (room !== path.curr && path.timeLeft - currPrices[room] > 1) {
+				madeNewPath = true;
+				paths.push({
+					curr: room,
+					toVisit: path.toVisit.filter((v) => v != room),
+					timeLeft: path.timeLeft - currPrices[room] - 1,
+					finished: false,
+					steps: [...path.steps, room],
+					finalPressure:
+						path.finalPressure +
+						(path.timeLeft - currPrices[room] - 1) * roomsFromName[room].flow,
+				});
+			}
+		});
+		if (!madeNewPath) path.finished = true;
+	}
+
+	return paths.filter((p) => p.finished).sort(sortPathByPressure)[0]
+		.finalPressure;
+};
+
+const dijkstra = (
+	start: Room,
+	endPositions: Room[],
+	map: InputRoomMap
+): CostMap => {
+	const visited: string[] = [];
+	const toVisit: Room[] = [start];
+	const lowestCost: CostMap = {
+		[start.name]: 0,
+	};
+
+	let curr;
+	while ((curr = toVisit.shift())) {
+		if (visited.includes(curr.name)) continue;
+
+		const worthItAdj = curr.adj
+			.filter((neighbour) => !visited.includes(neighbour))
+			.map((neighbour) => map[neighbour]);
+
+		toVisit.push(...worthItAdj);
+
+		const costToCurr = lowestCost[curr.name];
+
+		worthItAdj.forEach((neighbour) => {
+			const newCostToNeighbour = costToCurr + 1;
+			const costToNeighbour =
+				lowestCost[neighbour.name] === undefined
+					? newCostToNeighbour
+					: lowestCost[neighbour.name];
+
+			if (newCostToNeighbour <= costToNeighbour) {
+				lowestCost[neighbour.name] = newCostToNeighbour;
+			}
+		});
+
+		visited.push(curr.name);
+	}
+
+	return endPositions.reduce((map: CostMap, room: Room) => {
+		return {
+			...map,
+			[room.name]: lowestCost[room.name],
+		};
+	}, {});
+};
+
+const input: Room[] = fs
+	.readFileSync('inputs/day16.txt', 'utf8')
 	.split('\n')
 	.filter((line) => line.length > 0)
-	.map((line) => {
-		const regex = /(x=(-?\d+), y=(-?\d+))/g;
-		const match1 = regex.exec(line) || [];
-		const sensorPos = [
-			match1[2] ? Number(match1[2]) : 0,
-			match1[3] ? Number(match1[3]) : 0,
-		] as Position;
-		const match2 = regex.exec(line) || [];
-		const beaconPos = [
-			match2[2] ? Number(match2[2]) : 0,
-			match2[3] ? Number(match2[3]) : 0,
-		] as Position;
+	.map((l) => {
+		const line = l.split(' ');
+		const name = line[1];
+		const adj = line.slice(9).map((v) => v.replace(',', ''));
+		const flow = Number(line[4].split('=')[1].slice(0, -1));
 		return {
-			pos: sensorPos,
-			beacon: beaconPos,
-			view:
-				Math.abs(sensorPos[0] - beaconPos[0]) +
-				Math.abs(sensorPos[1] - beaconPos[1]),
-		} as Sensor;
+			name: name,
+			flow: flow,
+			adj: adj,
+		} as Room;
 	});
 
-const hiddenPoints = sensors.flatMap((sensor) => {
-	const peri = getPerimetre(sensor);
-	const howManySensorsSeeThis: (Position | undefined)[] = peri.map((point) => {
-		const whichSensorSeesThis = sensors.findIndex((sensor) => {
-			return calculateDistance(sensor.pos, point) <= sensor.view;
-		});
-		if (whichSensorSeesThis === -1) {
-			if (
-				point[0] >= 0 &&
-				point[0] <= maxCoord &&
-				point[1] >= 0 &&
-				point[1] <= maxCoord
-			)
-				return point;
-			return undefined;
-		} else {
-			return undefined;
-		}
-	});
-	return howManySensorsSeeThis.filter((p) => p !== undefined);
-});
+const inputRooms: InputRoomMap = input.reduce(
+	(map, currRoom) => ({
+		...map,
+		[currRoom.name]: currRoom,
+	}),
+	{} as InputRoomMap
+);
 
-const distressBeaconPosition = hiddenPoints[0] as Position;
+const startingRoom = inputRooms['AA'];
 
-const answer = distressBeaconPosition[0] * maxCoord + distressBeaconPosition[1];
+const startingRooms = [startingRoom, ...input.filter((room) => room.flow != 0)];
+const destinationRooms = input.filter((room) => room.flow != 0);
+
+const roomsMovePrices: PricesRoomMap = startingRooms.reduce(
+	(map: PricesRoomMap, room: Room) => {
+		return {
+			...map,
+			[room.name]: dijkstra(
+				room,
+				destinationRooms.filter((r) => r.name != room.name),
+				inputRooms
+			),
+		};
+	},
+	{} as PricesRoomMap
+);
+
+const answer = getMaxPressure(
+	30,
+	destinationRooms,
+	roomsMovePrices,
+	inputRooms
+);
 
 console.log(answer);
