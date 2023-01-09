2bbcea701bb464d2cd3822ea386ef556ba3b11cb
choeqq <k.chola@op.pl>
2023-01-09 23:26:43 +0100

    day 10 part 2 fix

--- src/2022/10/1.ts
+++ src/2022/10/1.ts
@@ -1,27 +1,50 @@
-import fs from 'fs';
+import * as fs from 'fs';
+import decode from './ocr';
 
-const input = fs.readFileSync('input.txt', 'utf8');
-const inputArray = input.split('\n');
+const WIDHT = 40;
+const HEIGHT = 6;
+const CHARACTERS = 8;
 
-let X = 1;
-let cycle = 1;
-const checkCycles = [20, 60, 100, 140, 180, 220];
-const signals: number[] = [];
+const calculatePixels = (register: number, position: number): string => {
+	const currPos = position % 40;
+	const lit = [register - 1, register, register + 1];
 
-inputArray.forEach((line) => {
-	const [cmd, nr] = line.split(' ');
+	return lit.includes(currPos) ? '#' : '.';
+};
 
-	const cycleTime = cmd === 'noop' ? 1 : 2;
+const applyInstruction = (history: number[], command: string): number[] => {
+	const [instruction, V] = command.split(' ');
+	const lastValue = history[history.length - 1];
+	const newValue = Number(V) + lastValue;
 
-	for (let i = 0; i < cycleTime; i++) {
-		if (cmd === 'addx' && i === 1) {
-			X += +nr;
-		}
-		cycle++;
-		if (checkCycles.includes(cycle)) {
-			signals.push(X * cycle);
-		}
-	}
-});
+	if (instruction === 'noop') return [...history, lastValue];
 
-console.log(signals.reduce((a, b) => a + b, 0));
+	return [...history, lastValue, newValue];
+};
+
+const input = fs
+	.readFileSync('input.txt', 'utf8')
+	.split('\n')
+	.filter((l) => l.length > 0);
+const regValues = input.reduce(applyInstruction, [1]).slice(0, WIDHT * HEIGHT);
+const litPixels = regValues.map(calculatePixels);
+const chars = Array(CHARACTERS)
+	.fill(0)
+	.map((_, i) =>
+		Array(HEIGHT)
+			.fill(0)
+			.map((_, j) =>
+				litPixels.slice(
+					i * (HEIGHT - 1) + j * WIDHT,
+					(i + 1) * (HEIGHT - 1) + j * WIDHT
+				)
+			)
+			.map((a) => a.join(''))
+	);
+
+const answer = chars
+	.map(decode)
+	.map((c) => c.toUpperCase())
+	.join('');
+
+console.log(answer);
