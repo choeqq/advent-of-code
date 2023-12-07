c74dbb054832c97843110ac61f139e480ee96743
choeqq <k.chola@op.pl>
2023-12-07 16:17:21 +0100

    day 1 part 1

--- src/2023/1/1.ts
+++ src/2023/1/1.ts
@@ -1,8 +1,16 @@
 import * as fs from 'fs';
 
-const input = fs.readFileSync('input.txt', 'utf8');
+const input = fs.readFileSync('input.txt', 'utf-8');
 const inputArray = input.split('\n');
 
-inputArray.forEach((line) => {
-	// Do your magic here with each line.
+const values = inputArray.map((line) => {
+	const first = line.split('').find((item) => !isNaN(+item));
+	const last = line
+		.split('')
+		.reverse()
+		.find((item) => !isNaN(+item));
+
+	return Number(first + last);
 });
+
+console.log(values.reduce((acc, cur) => acc + cur, 0));
