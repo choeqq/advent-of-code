2cec29028837482325b24d075598d6af9dba03ba
choeqq <kacper.chola01@gmail.com>
2023-12-24 23:00:56 +0100

    day 10 part 1, example fix

---
+++ src/2023/10/1.ts
@@ -0,0 +1,8 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('input.txt', 'utf8');
+const inputArray = input.trim().split('\n');
+
+inputArray.forEach((line) => {
+	// Do your magic here with each line.
+});
