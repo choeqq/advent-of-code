33f931b4ab0dc4e6ec0573b22927555c8ffa8193
choeqq <kacper.chola01@gmail.com>
2023-12-26 18:37:29 +0100

    day 11 part 1

---
+++ src/2023/11/1.ts
@@ -0,0 +1,153 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('input.txt', 'utf8');
+const inputArray = input.trim().split('\n');
+
+interface Monkey {
+	id: number;
+	startingItems: number[];
+	operation: string;
+	operand: number;
+	testDivisible: number;
+	toMonkeyIfTestTrue: number;
+	toMonkeyIfTestFalse: number;
+}
+
+const monkeys: Monkey[] = [];
+
+const NUM_MONKEYS = 8;
+const NUM_LINES_PER_MONKEY = 7;
+
+// parse file
+for (let i = 0; i < NUM_MONKEYS; i++) {
+	let id;
+	let startingItems;
+	let operation;
+	let operand;
+	let testDivisible;
+	let toMonkeyIfTestTrue;
+	let toMonkeyIfTestFalse;
+	for (let j = 0; j < NUM_LINES_PER_MONKEY; j++) {
+		const line = inputArray[i * NUM_LINES_PER_MONKEY + j];
+		if (j === 0) {
+			id = parseInt(line[7], 10);
+		} else if (j === 1) {
+			const numbersStr = line.substring(line.indexOf(': ') + 2);
+			startingItems = numbersStr
+				.split(', ')
+				.map((numberStr) => parseInt(numberStr, 10));
+		} else if (j === 2) {
+			const indexOfOperation =
+				line.indexOf('*') !== -1 ? line.indexOf('*') : line.indexOf('+');
+			operation = line[indexOfOperation];
+			operand = parseInt(line.substring(25), 10);
+			if (isNaN(operand)) {
+				operand = -1;
+			}
+		} else if (j === 3) {
+			testDivisible = parseInt(line.substring(21), 10);
+		} else if (j === 4) {
+			toMonkeyIfTestTrue = parseInt(line.substring(29), 10);
+		} else if (j === 5) {
+			toMonkeyIfTestFalse = parseInt(line.substring(30), 10);
+		}
+	}
+	monkeys[i] = {
+		id,
+		startingItems,
+		operation,
+		operand,
+		testDivisible,
+		toMonkeyIfTestTrue,
+		toMonkeyIfTestFalse,
+	};
+}
+console.log({ monkeys });
+
+/*
+
+*/
+
+monkeys.forEach((monkey) => {
+	console.log(monkey.startingItems);
+});
+
+// tracker for number of inspections
+const monkeyMap = new Map<number, number>();
+/*
+PART TWO MODULUS MATH
+Want to divide by some number such that test condition will still be satisfied.
+Without reducing worry levels, we will exceed Javascript Max Safe Integer
+Number.MAX_SAFE_INTEGER
+9007199254740991
+
+modulo by the product of all test numbers
+research: congruence modulus.
+
+Example: monkey 1 and 3, test is divisible by 3 and 11.
+3 * 11 = 33
+
+use %33 to determine if a number is divisible by 3.
+15 % 33 = 15, 150 % 33 = 18, 300 % 33 = 3 -> all divisible by 3!
+200 % 33 = 66.667 -> not divisible by 3!
+
+use %33 to determine if a number is divisible by 11.
+44 % 33 = 11, 121 % 33 = 22 -> both disivible by 11.
+111 % 33 = 12 -> not divisible by 11. but is by 3 ;)
+
+Therefore -> use the product of all test divisors to modulo the worry level!
+*/
+let mod = 1; // part 2
+monkeys.forEach((monkey) => {
+	monkeyMap.set(monkey.id, 0);
+	mod *= monkey.testDivisible; // part 2
+});
+console.log({ mod }); // part 2
+
+for (let round = 0; round < 10000; round++) {
+	for (const monkey of monkeys) {
+		// get items and loop through them
+		const items = monkey.startingItems;
+		for (let itemUnderInspection of items) {
+			// increment number of inspections for this monkey by 1.
+			monkeyMap.set(monkey.id, monkeyMap.get(monkey.id) + 1);
+			if (monkey.operation === '*') {
+				if (monkey.operand !== -1) {
+					// multiply worry level by amount
+					itemUnderInspection *= monkey.operand;
+				} else {
+					// multiply worry level by self (itemUnderInspection). i.e. old * old
+					itemUnderInspection *= itemUnderInspection;
+				}
+			} else if (monkey.operation === '+') {
+				if (monkey.operand !== -1) {
+					itemUnderInspection += monkey.operand;
+				} else {
+					itemUnderInspection += itemUnderInspection;
+				}
+			}
+
+			// part two mod
+			itemUnderInspection %= mod;
+
+			// decide which monkey to throw
+			if (itemUnderInspection % monkey.testDivisible === 0) {
+				monkeys[monkey.toMonkeyIfTestTrue].startingItems.push(
+					itemUnderInspection,
+				);
+			} else {
+				monkeys[monkey.toMonkeyIfTestFalse].startingItems.push(
+					itemUnderInspection,
+				);
+			}
+		}
+		// monkey has thrown all items - set their startingItems to empty array.
+		monkey.startingItems = [];
+	}
+}
+
+monkeys.forEach((monkey) => {
+	console.log(monkey.startingItems);
+});
+
+console.log([...monkeyMap].sort((a, b) => b[1] - a[1]));
